0c6ee363ed59385a90df7519eb4fe4614b1e79e7
Nikita <nikitaignatiev@gmail.com>
2013-03-28 10:01:52 +0400

    Initial commit

---
+++ DIS/DIS_Form.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace DIS
+{
+    partial class DIS
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DIS));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txBox_url = new System.Windows.Forms.TextBox();
+            this.btn_Download = new System.Windows.Forms.Button();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lbl_urlExample = new System.Windows.Forms.Label();
+            this.chBox_Spy = new System.Windows.Forms.CheckBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toTrayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(166, 53);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(33, 28);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Visible = false;
+            // 
+            // txBox_url
+            // 
+            this.txBox_url.Location = new System.Drawing.Point(12, 28);
+            this.txBox_url.Name = "txBox_url";
+            this.txBox_url.Size = new System.Drawing.Size(268, 20);
+            this.txBox_url.TabIndex = 1;
+            // 
+            // btn_Download
+            // 
+            this.btn_Download.Location = new System.Drawing.Point(205, 60);
+            this.btn_Download.Name = "btn_Download";
+            this.btn_Download.Size = new System.Drawing.Size(75, 23);
+            this.btn_Download.TabIndex = 2;
+            this.btn_Download.Text = "Dwnld";
+            this.btn_Download.UseVisualStyleBackColor = true;
+            this.btn_Download.Click += new System.EventHandler(this.btn_Download_Click);
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBar1.Step = 1;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // lbl_urlExample
+            // 
+            this.lbl_urlExample.AutoSize = true;
+            this.lbl_urlExample.Location = new System.Drawing.Point(12, 50);
+            this.lbl_urlExample.Name = "lbl_urlExample";
+            this.lbl_urlExample.Size = new System.Drawing.Size(139, 13);
+            this.lbl_urlExample.TabIndex = 4;
+            this.lbl_urlExample.Text = "http://2ch.hk/b/11111.html";
+            // 
+            // chBox_Spy
+            // 
+            this.chBox_Spy.AutoSize = true;
+            this.chBox_Spy.Checked = true;
+            this.chBox_Spy.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chBox_Spy.Location = new System.Drawing.Point(12, 66);
+            this.chBox_Spy.Name = "chBox_Spy";
+            this.chBox_Spy.Size = new System.Drawing.Size(68, 17);
+            this.chBox_Spy.TabIndex = 5;
+            this.chBox_Spy.Text = "Следить";
+            this.chBox_Spy.UseVisualStyleBackColor = true;
+            this.chBox_Spy.CheckedChanged += new System.EventHandler(this.chBox_Spy_CheckedChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "DIS";
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toTrayToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // toTrayToolStripMenuItem
+            // 
+            this.toTrayToolStripMenuItem.Name = "toTrayToolStripMenuItem";
+            this.toTrayToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.toTrayToolStripMenuItem.Text = "To tray";
+            this.toTrayToolStripMenuItem.Click += new System.EventHandler(this.toTrayToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(336, 24);
+            this.menuStrip.TabIndex = 6;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 134);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(336, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar2
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(109, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // DIS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 156);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.chBox_Spy);
+            this.Controls.Add(this.lbl_urlExample);
+            this.Controls.Add(this.btn_Download);
+            this.Controls.Add(this.txBox_url);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "DIS";
+            this.Text = "2ch_Download_Image";
+            this.Load += new System.EventHandler(this.DIS_Load);
+            this.Resize += new System.EventHandler(this.DIS_Resize);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txBox_url;
+        private System.Windows.Forms.Button btn_Download;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label lbl_urlExample;
+        private System.Windows.Forms.CheckBox chBox_Spy;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toTrayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+
+    }
+}
+
---
+++ DIS/DIS_Form.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DIS
+{
+    public partial class DIS : Form
+    {
+        string dirpath;
+        string filepath;
+        string[] sa;
+        const string waitAddressText = "Ожидаем ввода адреса.";
+        const string loadingFileText = "Загружаем файлы...";
+        const string allImageDownloaded = "Все изображения загружены.";
+        const string sourceText = "/src/";
+        const string dirDelimeter = "\\";
+        const string htmlTagNameText = "span";
+        const string htmlClass = "classname";
+        const string htmlAttribute = "filesize";
+        const char delimeterHtmlAddress = '/';
+        const string entrance2chFirst = "http://2ch.hk/";
+        const string entrance2chSecond = ".html";
+        private IntPtr nextClipboardViewer;
+        private const int WM_DRAWCLIPBOARD = 0x0308;
+        private const int WM_CHANGECBCHAIN = 0x030D;
+
+        public DIS()
+        {
+            InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+            toolStripStatusLabel1.Text = waitAddressText;
+            WindowState = FormWindowState.Minimized;
+
+        }
+
+        private void DIS_Load(object sender, EventArgs e)
+        {
+            chBox_Spy_CheckedChanged(sender, e);
+        }
+        
+        private void btn_Download_Click(object sender, EventArgs e)
+        {
+            downloadImage();
+        }
+
+        private void downloadImage()
+        {
+            if (txBox_url.Text != "")
+            {
+                webBrowser1.Url = new Uri(txBox_url.Text);
+                webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(WebBrowser_DocumnetCompleted);
+                sa = txBox_url.Text.Substring(7).Split(delimeterHtmlAddress);
+            }
+        }
+
+
+        private void WebBrowser_DocumnetCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            toolStripStatusLabel1.Text = loadingFileText;
+            HtmlElementCollection theElementCollection = default(HtmlElementCollection);
+            theElementCollection = webBrowser1.Document.GetElementsByTagName(htmlTagNameText);
+            dirpath = sa[1] + dirDelimeter + sa[3].Split('.')[0];
+            System.IO.Directory.CreateDirectory(dirpath);
+            toolStripProgressBar1.Maximum = theElementCollection.Count+1;
+
+            foreach (HtmlElement curElement in theElementCollection)
+            {
+                toolStripProgressBar1.Value++;
+                if (curElement.GetAttribute(htmlClass).ToString() == htmlAttribute)
+                {
+                    string s = "http://" + sa[0] + delimeterHtmlAddress + sa[1] + sourceText + curElement.InnerText.Split(' ')[0];
+                    System.Net.WebClient wc = new System.Net.WebClient();
+
+                    filepath = Environment.CurrentDirectory + dirDelimeter + dirpath + dirDelimeter + curElement.InnerText.Split(' ')[0];
+                    if (System.IO.File.Exists(filepath))
+                    {
+                        continue;
+                    }
+                    wc.DownloadFileAsync(new Uri(s), @filepath);
+                }
+
+            }
+            toolStripStatusLabel1.Text = allImageDownloaded;
+            toolStripProgressBar1.Value = 0;
+            notifyIcon1.ShowBalloonTip(300, "Уведомление", "Скачено.", ToolTipIcon.None);
+        }
+
+        private void statusStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("explorer.exe", @Environment.CurrentDirectory + dirDelimeter + dirpath);
+        }
+
+        private void chBox_Spy_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chBox_Spy.Checked)
+            {
+                nextClipboardViewer = SetClipboardViewer(this.Handle);
+            }
+            else
+            {
+                ChangeClipboardChain(this.Handle, nextClipboardViewer);
+            }
+        }
+
+
+        public string SwapClipboardHtmlText(string replacementHtmlText)
+        {
+            String returnHtmlText = null;
+            if (Clipboard.ContainsText(TextDataFormat.Html))
+            {
+                returnHtmlText = Clipboard.GetText(TextDataFormat.Html);
+            }
+            return returnHtmlText;
+        }
+
+
+        //Register a window handle as a clipboard viewer
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SetClipboardViewer(IntPtr hWnd);
+        //Remove a window handle from the clipboard viewers chain
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool ChangeClipboardChain(
+            IntPtr hWndRemove,  // handle to window to remove
+            IntPtr hWndNewNext  // handle to next window
+            );
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hwnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_DRAWCLIPBOARD:
+                    //the contents of the clipboard have changed
+                    {
+                        //Process clipboard change
+                        ClipboardChanged();
+                        //Send the message to the next window
+                        SendMessage(nextClipboardViewer, WM_DRAWCLIPBOARD, IntPtr.Zero, IntPtr.Zero);
+                        break;
+                    }
+                case WM_CHANGECBCHAIN:
+                    //the clipboard chain has changed and we have to pass the news along
+                    {
+                        if (m.WParam == nextClipboardViewer)
+                        {
+                            //the window we've been passing WM_DRAWCLIPBOARD to has been removed
+                            //from the chain, so we have to update our message target
+                            nextClipboardViewer = m.LParam;
+                        }
+                        else
+                        {
+                            //just pass along the message
+                            SendMessage(nextClipboardViewer, WM_CHANGECBCHAIN, m.WParam, m.LParam);
+                        }
+                        m.Result = IntPtr.Zero;
+                        break;
+                    }
+                default:
+                    {
+                        base.WndProc(ref m);
+                        break;
+                    }
+            }
+        }
+
+        private void ClipboardChanged()
+        {
+            if (Clipboard.ContainsText() && 
+                Clipboard.GetText().Substring(0, entrance2chFirst.Length) == entrance2chFirst && 
+                Clipboard.GetText().Substring(Clipboard.GetText().Length-entrance2chSecond.Length,entrance2chSecond.Length) == entrance2chSecond)
+            {
+                txBox_url.Text = Clipboard.GetText();
+
+                if (FormWindowState.Minimized == WindowState)
+                notifyIcon1.ShowBalloonTip(300, "Уведомление","Скачать?",ToolTipIcon.None);
+            }
+        }
+
+        private void toTrayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void DIS_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                notifyIcon1.Visible = true;
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            // Разворачиваем окно
+            WindowState = FormWindowState.Normal;
+            // Прячем за собой иконку в трее
+            notifyIcon1.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Можно скачать с github", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                downloadImage();
+        }
+
+        private void toTrayToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
